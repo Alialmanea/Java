fc833c77cdfa61c260e352fcad9a14eacab8ec0b
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2020-01-20 15:27:54 +0300

    Create Example of Class in java .java

---
+++ Example of Class in java .java
@@ -0,0 +1,28 @@
+package NYP;
+
+class Out{
+	
+	void WriteLine (String str ) {
+		
+		System.out.println(str);
+	}
+	
+}
+
+
+public class Console {
+	
+	static Out out =new Out();
+	
+
+	public static void main(String[] args) {
+		Console.out.WriteLine("hello world");
+	
+	
+
+		
+		
+		
+	}
+
+}
